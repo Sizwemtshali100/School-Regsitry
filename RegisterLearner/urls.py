0e6c6a304f7614d08de8190f3dfbab14b4e1312b
Sizwe <Sizwemtshali100@gmail.com>
2022-11-14 00:12:55 +0200

    Adding the grade 10, 11, and 12 + uncommented my url code

--- RegisterLearner/urls.py
+++ RegisterLearner/urls.py
@@ -4,9 +4,9 @@ urlpatterns = [
     path('', views.Home, name='Home'),
     path('Grade8/', views.Grade8, name='Grade8'),
     path('Grade9/', views.Grade9, name='Grade9'),
-    #path('Grade10/', views.Grade10, name='Grade10'),
-    #path('Grade11/', views.Grade11, name='Grade11'),
-    #path('Grade12/', views.Grade12, name='Grade12'),
+    path('Grade10/', views.Grade10, name='Grade10'),
+    path('Grade11/', views.Grade11, name='Grade11'),
+    path('Grade12/', views.Grade12, name='Grade12'),
     path('AllParent/', views.AllParent, name='AllParent'),
     path('StudentDetails/<int:pk_details>', views.StudentDetails, name='StudentDetails'),
     #CRUD
